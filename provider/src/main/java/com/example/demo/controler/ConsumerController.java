803e5ea70f8c1361b21440c3b635e5444bca3cac
Pencil-L <15047145261@163.com>
2020-05-12 11:57:05 +0800

    no message

---
+++ provider/src/main/java/com/example/demo/controler/ConsumerController.java
@@ -0,0 +1,26 @@
+package com.example.demo.controler;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+/**
+ * Created on 2020/5/12
+ *
+ * @author lixuanfeng
+ */
+@RestController
+@RequestMapping("/consumer")
+public class ConsumerController {
+
+    @Autowired
+    private RestTemplate restTemplate;
+
+    @RequestMapping("/hello")
+    public String hello(){
+        //调用spring cloud服务提供者提供的服务 调用远程服务的返回值类型
+        return  restTemplate.getForEntity("http://localhost:8080/provider/hello",String.class).getBody();
+    }
+
+}
---
+++ provider/src/main/java/com/example/demo/controler/ProviderController.java
@@ -0,0 +1,19 @@
+package com.example.demo.controler;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created on 2020/5/12
+ *
+ * @author lixuanfeng
+ */
+@RestController
+@RequestMapping("/provider")
+public class ProviderController {
+
+    @RequestMapping("/hello")
+    public String hello(){
+        return "spring cloud provider-01 hello world";
+    }
+}
